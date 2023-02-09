ca5a288c8ad2609778cc1aecd7b85df0c63ce049
Marisa Kirisame <stavtnr@gmail.com>
2023-02-09 13:25:36 +0300

    config/emacs updated

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -1,24 +1,28 @@
 (require 'package)
+
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/"))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-(setq custom-file "~/.config/emacs/custom.el")
+(setq custom-file "./custom.el")
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
 
+(setq backup-directory-alist '(("." . "~/tmp/")))
+
 (setq inhibit-startup-message t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-(global-display-line-numbers-mode 1)
+
 (column-number-mode 1)
 
-(setq backup-directory-alist '(("." . "~/tmp/")))
+(global-display-line-numbers-mode 1)
 
 (unless (package-installed-p 'moe-theme)
   (package-install 'moe-theme))
@@ -30,19 +34,6 @@
 ;; (load-theme 'moe-dark t)
 (load-theme 'abyss t)
 
-(global-set-key (kbd "C-M-i") 'completion-at-point)
-
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-(use-package evil
-  :ensure t
-  :config (evil-mode 1)
-          (evil-set-undo-system 'undo-tree))
-
-(use-package undo-tree
-  :ensure t
-  :config (global-undo-tree-mode 1))
-
 (use-package helm
   :ensure t
   :bind (("M-x" . helm-M-x)
@@ -52,9 +43,67 @@
          ("C-j" . helm-next-line)
          ("C-k" . helm-previous-line))
   :config (helm-mode 1)
-          (setq-default helm-M-x-fuzzy-match t)
+          (setq-default helm-M-x-fuzzy-match t))
 
-(setq org-startup-folded t)
+(setq evil-want-keybinding nil)
+
+(use-package evil-leader
+  :ensure t
+  :config (setq evil-leader/leader "<SPC>")
+          (evil-leader/set-key
+            "." 'helm-find-files
+            "b" 'helm-buffers-list
+            "k" 'kill-buffer))
+
+(use-package evil
+  :after evil-leader
+  :ensure t
+  :config (global-evil-leader-mode 1)
+          (evil-mode 1)
+          (evil-set-undo-system 'undo-tree))
+
+(use-package undo-tree
+  :ensure t
+  :config (global-undo-tree-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config (evil-collection-init))
+
+(use-package magit
+  :ensure t)
+
+(use-package vterm
+  :ensure t
+  :init (add-hook 'vterm-mode-hook (lambda() (display-line-numbers-mode 0))))
+
+(use-package feebleline
+  :ensure t
+  :config (setq feebleline-msg-functions
+	  '((feebleline-line-number         :post "" :fmt "%5s")
+	    (feebleline-column-number       :pre ":" :fmt "%-2s")
+	    (feebleline-file-directory      :face feebleline-dir-face :post "")
+	    (feebleline-file-or-buffer-name :face font-lock-keyword-face :post "")
+	    (feebleline-file-modified-star  :face font-lock-warning-face :post "")
+	    (feebleline-git-branch          :face feebleline-git-face :pre " : ")
+	    (feebleline-project-name        :align right)))
+          (feebleline-mode 1))
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode 1)
+  :config (setq which-key-idle-delay 0.3))
+
+(global-set-key (kbd "C-M-i") 'completion-at-point)
+
+(use-package lsp-mode
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode 1))
+
 (setq org-startup-indented t)
 (setq org-startup-with-inline-images nil)
 
@@ -66,45 +115,12 @@
   :ensure t
   :hook (org-mode . org-evil-mode))
 
-(use-package vterm
-  :ensure t)
-
-(use-package magit
-  :ensure t)
-
-(use-package pdf-tools
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :init (which-key-mode 1)
-  :config (setq which-key-idle-delay 0.3)
-  )
-
-(use-package feebleline
-  :ensure t
-  :config (setq feebleline-msg-functions
-                '((feebleline-line-number         :post "" :fmt "%5s")
-                  (feebleline-column-number       :pre ":" :fmt "%-2s")
-                  (feebleline-file-directory      :face feebleline-dir-face :post "")
-                  (feebleline-file-or-buffer-name :face font-lock-keyword-face :post "")
-                  (feebleline-file-modified-star  :face font-lock-warning-face :post "")
-                  (feebleline-git-branch          :face feebleline-git-face :pre " : ")
-                  (feebleline-project-name        :align right)))
-          (feebleline-mode 1))
-
-(use-package lsp-mode
-  :ensure t)
-
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode 1))
-
 (use-package auctex
   :ensure t
   :defer t)
 
-(setq TeX-view-program-selection '((output-pdf "PDF Tools")))
+(use-package pdf-tools
+  :ensure t)
 
 (use-package latex-preview-pane
   :ensure t)
